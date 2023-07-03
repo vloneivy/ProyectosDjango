93ae2a54390430e69fb45e973d1d2ae682112c39
paketenamore33 <130715670+paketenamore33@users.noreply.github.com>
2023-07-03 20:30:52 +0200

    borrado de proyecto antiguo
    lucas: borre el proyecto antiguo que estaba malo ;(

--- TiendaJuegos/core/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from django.forms import ModelForm, fields
-from .models import Vehiculo
-
-class VehiculoForm(ModelForm):
-    class Meta:
-        model = Vehiculo
-        fields = ['ID', 'marca', 'modelo', 'imagen', 'categoria']
